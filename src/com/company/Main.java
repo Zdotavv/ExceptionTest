ccc97f3e836b6750728649321bb7f2cbe633360c
Vladimir <zdotavv@ukr.net>
2022-04-01 18:39:50 +0300

    incompatible types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,8 @@
 package com.company;
 
 public class Main {
-    public double sqr(double arg) { // надо double
-        int k = 1;                  // есть int
-        return (double) k;          // явное преобразование int в double
+        public static double sqr(double arg) {
+            return "hello!";
+        }
     }
-}
+//Другой тип.incompatible types: java.lang.String cannot be converted to double
