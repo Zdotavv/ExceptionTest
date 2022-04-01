af0d4ad6bcbe65ffaf14c7e2d66f8d9cf1f3b882
Vladimir <zdotavv@ukr.net>
2022-04-01 18:34:46 +0300

    double and double

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,7 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("sout");
-        throw new Error();
+    public double sqr(double arg) { // надо double
+        return arg * arg;           // double * double - это double
     }
 }
-//сли выводим сообщение в System.out, то на это тратится каке-то время для буферизации,
-// а , если, в System.err, то сообщение сразу выводится в консоль,
-// из-за чего System.err может обогнать System.out
