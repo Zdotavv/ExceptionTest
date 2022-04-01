47c291f18b1aface4c4af2b39926c2bf646b2f80
Vladimir <zdotavv@ukr.net>
2022-04-01 18:55:41 +0300

    while (true)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,8 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        double d = sqr(10.0); // ну, и чему равно d?
-        System.out.println(d);
     }
-    public static double sqr(double arg) {
-        // nothing
+        public static double sqr(double arg) {
+            while (true); // Удивительно, но КОМПИЛИРУЕТСЯ!
+        }
     }
-}
-// missing return statement
