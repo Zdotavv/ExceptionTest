eea35ae348d3f6489f175672f39f5f29b282e0bf
Vladimir <zdotavv@ukr.net>
2022-04-01 18:44:05 +0300

    missing return statement currentTimeMillis()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
     public static double sqr(double arg) {
+        if (System.currentTimeMillis() % 2 == 0) {
+            return arg * arg; // если currentTimeMillis() - четное число, то все ОК
+        }
+        // а если нечетное, что нам возвращать?
     }
 }
-//Нет возврата. missing return statement
+// missing return statement
