f15267ab7f3965f983cfd2dff222983ba9098217
Vladimir <zdotavv@ukr.net>
2022-04-01 17:40:38 +0300

    Error ref = new Error() Exception

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,8 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        throw null;
-        //not ok
-        // Exception in thread "main" java.lang.NullPointerException: Cannot throw exception because "null" is null
-        //	at com.company.Main.main(Main.java:5)
+        Error ref = new Error(); // создаем экземпляр
+        throw ref;               // "бросаем" его
+        //Exception in thread "main" java.lang.Error
     }
 }
