baa258d67f58e7dbf452f8cd48c309bc62f967e2
Vladimir <zdotavv@ukr.net>
2022-04-01 16:24:42 +0300

    catch with String S not work

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         try {
-        } catch (Throwable t) {}
+        } catch (String s) {}  //java: incompatible types: java.lang.String cannot be converted to java.lang.Throwable
     }
 }
