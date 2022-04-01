0b079de747b61e74c55ccf8085f91d5a7bed0db4
Vladimir <zdotavv@ukr.net>
2022-04-01 18:45:38 +0300

    missing return statement double sqr

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
 public class Main {
+    public static void main(String[] args) {
+        double d = sqr(10.0); // ну, и чему равно d?
+        System.out.println(d);
+    }
     public static double sqr(double arg) {
-        if (System.currentTimeMillis() % 2 == 0) {
-            return arg * arg; // если currentTimeMillis() - четное число, то все ОК
-        }
-        // а если нечетное, что нам возвращать?
+        // nothing
     }
 }
 // missing return statement
