11e8dc6fd9c08f87c14372d814ed98a0d64dc705
Vladimir <zdotavv@ukr.net>
2022-04-01 16:18:58 +0300

    catch with Throwable work

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
 public class Main {
-    public static void main(String[] args) throws String {}
+    public static void main(String[] args) {
+        try {
+        } catch (Throwable t) {}
+    }
 }
