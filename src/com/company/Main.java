6103c70e3f37c2265db8b4fa1da9d9957c74a5d2
Vladimir <zdotavv@ukr.net>
2022-04-01 19:12:29 +0300

    throw new RuntimeException();

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,7 @@ public class Main {
     public static void main(String[] args) {
 sqr(10);
     }
-        public static double sqr(double arg) {
-            if (System.currentTimeMillis() % 2 == 0) {
-                return arg * arg; // ну ладно, вот твой double
-            } else {
-                while (true);     // а тут "виснем" навсегда
-            }
-        }
+    public static double sqr(double arg) {
+        throw new RuntimeException();
     }
+}
