d0c7f17f5a9b9060001c3336d67dcb3fc73d86b2
Vladimir <zdotavv@ukr.net>
2022-04-01 17:50:38 +0300

    f(null) StackOverflowError

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,15 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Error ref = new Error(); // создаем экземпляр
-        throw ref;               // "бросаем" его
-        //Exception in thread "main" java.lang.Error
+        f(null);
+    }
+    public static void f(NullPointerException e) {
+        try {
+            throw e;
+        } catch (NullPointerException npe) {
+            f(npe);
+        }
     }
 }
+//Передавая null, и, вызывая NullPointerException, ловим его catch. Затем снова вызываем этот метод передавая ему NullPointerException.
+//В резултате стек переполняется. Exception in thread "main" java.lang.StackOverflowError
