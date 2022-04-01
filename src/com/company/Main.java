06dd649d2dfb7bbdec190ec1ed2ace20ed3f6150
Vladimir <zdotavv@ukr.net>
2022-04-01 19:08:19 +0300

    while (true)3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,13 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        double d = sqr(10.0);  // sqr - навсегда "повиснет", и
-        System.out.println(d); // d - НИКОГДА НИЧЕГО НЕ БУДЕТ ПРИСВОЕНО!
+sqr(10);
     }
-    public static double sqr(double arg) {
-        while (true); // Вот тут мы на века "повисли"
+        public static double sqr(double arg) {
+            if (System.currentTimeMillis() % 2 == 0) {
+                return arg * arg; // ну ладно, вот твой double
+            } else {
+                while (true);     // а тут "виснем" навсегда
+            }
+        }
     }
-}
